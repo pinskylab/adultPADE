b4db99a93eb80ecf84d3eeb755a2a5f9b93e16d4
Ryan Snow <snow4964@gmail.com>
2023-04-04 23:29:34 -0400

    Additional potential helper scripts uploaded

---
+++ HelperScripts/remove.bad.hap.loci.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z "$2" ]; then
+echo "Correct usage is sh remove.bad.hap.loci.sh file_with_bad_Loci vcf_file"
+exit 1
+fi
+
+NAME=$(echo $2 | sed -e 's/\.recode.*//g') 
+
+grep -vwf <(cut -f1 $1) $2 > $NAME.filtered.vcf
